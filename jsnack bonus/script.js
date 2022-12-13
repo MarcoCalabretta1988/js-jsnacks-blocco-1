e26277b36f5daf711324ce4ac011056ace436ac3
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2022-12-13 15:22:44 +0100

    add first script

--- jsnack bonus/script.js
+++ jsnack bonus/script.js
@@ -1,3 +1,45 @@
 // LINK JS OK
 
 console.log(' JS OK');
+
+
+//! TRACCIA
+/**
+ L'utente inserisce un numero di 4 cifre in un input. Calcola la somma di tutte le cifre che compongono il numero e stampala in pagina. 
+ */
+
+// prendo Elementi dal dom
+
+const button = document.getElementById('button');
+const userNumber = document.getElementById('number');
+const result = document.getElementById('result');
+
+
+//Creo logica a evento click del bottone
+
+button.addEventListener('click' , function(){
+
+//prendo il valore inserito dall utente dal form
+
+const numberToSum = userNumber.value.trim();
+
+const lunghezza = numberToSum.lenght;
+console.log(numberToSum);
+console.log(lunghezza);
+
+//validazione 
+
+if (numberToSum.lenght < 4 || numberToSum.lenght > 4){
+    result.innerText = ' Hai inserito un valore errato';
+}
+else{
+    let sum = 0;
+    for(let i = 0 ; i < numberToSum.lenght ; i++ ){
+        sum += numberToSum[i];
+    }
+    result.innerText = ` La somma dei tuoi valori è: ${sum}`;
+}
+
+
+
+});
